abb06121d5273358b09e3c2bf943f1f34846f50f
Artur Pedziwilk <wilkart@mac.com>
2020-12-02 18:22:52 +0100

    fix the fix

--- Formula/gridcoin-cli@db4.rb
+++ Formula/gridcoin-cli@db4.rb
@@ -1,4 +1,4 @@
-class GridcoinCli@db4 < Formula
+class GridcoinCliATDb4 < Formula
   desc "Gridcoin Research CLI for macOS"
   homepage "https://gridcoin.us/"
   url "https://github.com/gridcoin-community/Gridcoin-Research/archive/5.1.0.0.tar.gz"
