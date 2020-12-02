c96f2e7bf9a83dd879145c0302c13f9e96aa4b79
Artur Pedziwilk <wilkart@mac.com>
2020-12-02 17:53:03 +0100

    remove strip

--- Formula/gridcoin-cli.rb
+++ Formula/gridcoin-cli.rb
@@ -56,7 +56,6 @@ class GridcoinCli < Formula
     system "./autogen.sh"
     system "unset OBJCXX ; ./configure --with-incompatible-bdb --without-gui --disable-tests --disable-bench --disable-dependency-tracking --disable-asm"
     system "make"
-    system "strip", "src/gridcoinresearchd"
     bin.install "src/gridcoinresearchd"
   end
 
